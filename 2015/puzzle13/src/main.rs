f460763076caf3c63d9ced5e2853855b3c3a5c24
Alireza Pishdar <pishdaralirza@gmail.com>
2023-01-23 23:15:20 +0330

    2015/puzzle13 Part Two

--- 2015/puzzle13/src/main.rs
+++ 2015/puzzle13/src/main.rs
@@ -1,6 +1,5 @@
-use std::collections::HashMap;
 use itertools::Itertools;
-
+use std::collections::HashMap;
 
 #[derive(Debug)]
 struct Edge {
@@ -10,12 +9,11 @@ struct Edge {
 
 impl Edge {
     fn new(person: String, happiness: i32) -> Self {
-        Self {person, happiness}
+        Self { person, happiness }
     }
 }
 
 type Graph = HashMap<String, Vec<Edge>>;
-
 
 fn make_graph(lines: Vec<String>) -> Graph {
     let mut graph: Graph = HashMap::new();
@@ -25,29 +23,29 @@ fn make_graph(lines: Vec<String>) -> Graph {
         let mut p2 = splited_line[10].to_string();
         p2.pop().unwrap();
         let mut happiness = splited_line[3].parse::<i32>().unwrap();
-        if splited_line[2] == "lose" { happiness = -happiness; }
+        if splited_line[2] == "lose" {
+            happiness = -happiness;
+        }
         match graph.get_mut(&p2) {
             None => (),
             Some(ref mut edges) => {
                 for e in edges.iter_mut() {
-                    if &e.person == &p1 {
+                    if e.person == p1 {
                         happiness += e.happiness;
                         e.happiness = happiness;
                     }
                 }
             }
         }
-        graph.entry(p1).and_modify(
-            |v| v.push(Edge::new(p2.clone(), happiness)))
-                .or_insert(vec![Edge::new(p2.clone(), happiness)]
-        );
+        graph
+            .entry(p1)
+            .and_modify(|v| v.push(Edge::new(p2.clone(), happiness)))
+            .or_insert_with(|| vec![Edge::new(p2.clone(), happiness)]);
     }
     graph
 }
 
-
-fn run(lines: Vec<String>) {
-    let graph = make_graph(lines);
+fn happiness_calc(graph: &Graph) -> i32 {
     let keys = graph.keys().map(|k| k.to_string()).collect::<Vec<String>>();
     let mut len = keys.len();
     let person_perms = keys.into_iter().permutations(len);
@@ -67,7 +65,7 @@ fn run(lines: Vec<String>) {
             i += 1;
             j += 1;
         }
-        let last_person = persons[len-1].as_str();
+        let last_person = persons[len - 1].as_str();
         for edge in &graph[last_person] {
             if edge.person == persons[0] {
                 h += edge.happiness;
@@ -78,16 +76,32 @@ fn run(lines: Vec<String>) {
             max_happiness = h;
         }
     }
-    println!("Part One: {}", max_happiness);
+    max_happiness
 }
 
+fn run(lines: Vec<String>) {
+    let mut graph = make_graph(lines);
+    let part_one = happiness_calc(&graph);
+    println!("Part One: {}", part_one);
+
+    for (_, edges) in graph.iter_mut() {
+        edges.push(Edge::new("me".to_string(), 0));
+    }
+    graph.insert("me".to_string(), vec![]);
+    let keys = graph.keys().map(|k| k.to_string()).collect::<Vec<String>>();
+    for k in keys {
+        graph.get_mut("me").map(|edges| edges.push(Edge::new(k, 0)));
+    }
+    let part_two = happiness_calc(&graph);
+    println!("Part Two: {}", part_two);
+}
 
 fn main() {
     let lines = std::fs::read_to_string("13")
-            .expect("can not read file 13")
-            .split('\n')
-            .map(|line| line.trim().to_string())
-            .collect::<Vec<String>>();  
+        .expect("can not read file 13")
+        .split('\n')
+        .map(|line| line.trim().to_string())
+        .collect::<Vec<String>>();
 
     run(lines);
 }
