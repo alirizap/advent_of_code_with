3a46848262b7f1b85ec758c337920504bbeed97a
Alireza Pishdar <pishdaralirza@gmail.com>
2023-01-28 08:56:20 +0330

    2016/puzzle01 part two

--- 2016/puzzle01/src/main.rs
+++ 2016/puzzle01/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Copy, Clone)]
 enum Dir {
     N,
     S,
@@ -5,32 +6,101 @@ enum Dir {
     E,
 }
 
-struct Pos(i32, i32);
+#[derive(Copy, Clone)]
+struct Pos {
+    face: Dir,
+    x: i32,
+    y: i32,
+}
 
-fn run(directions: Vec<&str>) {
-    let mut curr_dir = Dir::N;
-    let mut curr_pos = Pos(0, 0);
+impl PartialEq for Pos {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
 
-    for d in directions.iter() {
-        let step = d[1..].parse::<i32>().unwrap();
-        if d.starts_with('L') {
-            match curr_dir {
-                Dir::N => { curr_dir = Dir::W; curr_pos.0 = curr_pos.0 - step; }
-                Dir::E => { curr_dir = Dir::N; curr_pos.1 = curr_pos.1 + step; }
-                Dir::S => { curr_dir = Dir::E; curr_pos.0 = curr_pos.0 + step; }
-                Dir::W => { curr_dir = Dir::S; curr_pos.1 = curr_pos.1 - step; }
+impl Pos {
+    fn new() -> Self {
+        Self {
+            face: Dir::N,
+            x: 0,
+            y: 0,
+        }
+    }
+
+    fn change_dir(&mut self, d: &str) {
+        self.face = match self.face {
+            Dir::N => {
+                if d.starts_with('L') {
+                    Dir::W
+                } else {
+                    Dir::E
+                }
             }
-        } else if d.starts_with('R') {
-            match curr_dir {
-                Dir::N => { curr_dir = Dir::E; curr_pos.0 = curr_pos.0 + step; }
-                Dir::E => { curr_dir = Dir::S; curr_pos.1 = curr_pos.1 - step; }
-                Dir::S => { curr_dir = Dir::W; curr_pos.0 = curr_pos.0 - step; }
-                Dir::W => { curr_dir = Dir::N; curr_pos.1 = curr_pos.1 + step; }
+            Dir::E => {
+                if d.starts_with('L') {
+                    Dir::N
+                } else {
+                    Dir::S
+                }
+            }
+            Dir::S => {
+                if d.starts_with('L') {
+                    Dir::E
+                } else {
+                    Dir::W
+                }
+            }
+            Dir::W => {
+                if d.starts_with('L') {
+                    Dir::S
+                } else {
+                    Dir::N
+                }
             }
         }
     }
 
-    println!("Part One: {}", curr_pos.0.abs() + curr_pos.1.abs());
+    fn walk(&mut self) {
+        match self.face {
+            Dir::N => self.y += 1,
+            Dir::S => self.y -= 1,
+            Dir::W => self.x -= 1,
+            Dir::E => self.x += 1,
+        }
+    }
+
+    fn position(&self) -> i32 {
+        self.x.abs() + self.y.abs()
+    }
+}
+
+fn run(directions: Vec<&str>) {
+    let mut curr_pos = Pos::new();
+
+    let mut visited: Vec<Pos> = Vec::new();
+    let mut twice_visited = Pos::new();
+    let mut found = false;
+
+    for d in directions.iter() {
+        let step = d[1..].parse::<i32>().unwrap();
+        curr_pos.change_dir(d);
+        for _ in 1..step + 1 {
+            curr_pos.walk();
+            if !found {
+                match visited.iter().find(|x| **x == curr_pos) {
+                    None => visited.push(curr_pos),
+                    Some(pos) => {
+                        twice_visited = *pos;
+                        found = true;
+                    }
+                }
+            }
+        }
+    }
+
+    println!("Part One: {}", curr_pos.position());
+    println!("Part Two: {}", twice_visited.position());
 }
 
 fn main() {
